b605e34df25be9ff76b522d03ccaaf23366be6af
Almaz Saidov <almaz.saidov.03@bk.ru>
2026-01-27 14:57:57 +0300

    Подключить ws-endpoint и раздачу статики

--- src/main.py
+++ src/main.py
@@ -2,8 +2,14 @@ import uvicorn
 from fastapi import FastAPI
 
 from core.config import settings
+from core.static import setup_static_files
+from websoket.router import router as ws_router
 
 app = FastAPI()
+
+app.include_router(ws_router)
+
+setup_static_files(app)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host=settings.HOST, port=settings.PORT)
