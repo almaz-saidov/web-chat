5aa44d9590a6229093fed6af4a6b6c57c68df37d
Almaz Saidov <almaz.saidov.03@bk.ru>
2026-01-30 18:25:30 +0300

    Вынести поулчение сервиса в Depends

--- src/api/routes/websocket.py
+++ src/api/routes/websocket.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, Depends, WebSocket
 
-from services.websoket_service import get_websocket_service
+from services.websoket_service import WebSocketService, get_websocket_service
 
 router = APIRouter()
 
 
 @router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket, username: str = "Guest"):
-    websocket_service = get_websocket_service()
+async def websocket_endpoint(
+    websocket: WebSocket,
+    username: str = "Guest",
+    websocket_service: WebSocketService = Depends(get_websocket_service),
+):
     await websocket_service.handle_websocket(websocket, username)
