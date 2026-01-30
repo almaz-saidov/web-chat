963924b62a9a8fbd568169ec6ba4a0a2479ee287
Almaz Saidov <almaz.saidov.03@bk.ru>
2026-01-30 12:25:08 +0300

    Выделить логику подключения и отключения в отдельные методы

--- src/services/websoket_service.py
+++ src/services/websoket_service.py
@@ -11,20 +11,31 @@ class WebSocketService:
         websocket: WebSocket,
         username: str,
     ) -> None:
-        await manager.connect(websocket, username)
-        await manager.broadcast(f"{username} joined the chat.")
+        await self._connect(websocket, username)
 
         try:
             while True:
                 data = await websocket.receive_text()
-
                 await manager.broadcast(f"{username}: {data}")
 
         except WebSocketDisconnect:
-            disconnected_user = manager.disconnect(websocket)
-            if disconnected_user:
-                await manager.broadcast(f"{disconnected_user} left the chat.")
+            await self._disconnect(websocket)
 
+    async def _connect(
+        self,
+        websocket: WebSocket,
+        username: str,
+    ) -> None:
+        await manager.connect(websocket, username)
+        await manager.broadcast(f"{username} joined the chat.")
+
+    async def _disconnect(
+        self,
+        websocket: WebSocket,
+    ) -> None:
+        disconnected_user = manager.disconnect(websocket)
+        if disconnected_user:
+            await manager.broadcast(f"{disconnected_user} left the chat.")
 
 
 @lru_cache
