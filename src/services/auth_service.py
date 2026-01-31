a432d48d6c69b2875bdd478c422e69ceefbdb184
Almaz Saidov <almaz.saidov.03@bk.ru>
2026-01-31 17:07:48 +0300

    Перенести функции для работы с паролями в сервис

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,10 +1,10 @@
+import bcrypt
 from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api.schemas import UserCreate
 from database.models import User
 from services.user_service import get_user_service
-from services.utils import hash_password
 
 
 class AuthService:
@@ -25,8 +25,19 @@ class AuthService:
 
         return await user_service.create_user({
             "username": user_data.username,
-            "password_hash": hash_password(user_data.password),
+            "password_hash": self._hash_password(user_data.password),
         })
+
+    def _hash_password(self, password: str) -> str:
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password.decode('utf-8')
+
+    def _verify_password(self, plain_password: str, hashed_password: str) -> bool:
+        return bcrypt.checkpw(
+            plain_password.encode('utf-8'),
+            hashed_password.encode('utf-8'),
+        )
 
 
 def get_auth_service(session: AsyncSession) -> AuthService:
