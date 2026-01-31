87730f0abebf367fd432a1be4ee56e86ed966c7d
Almaz Saidov <almaz.saidov.03@bk.ru>
2026-01-31 19:06:38 +0300

    Сделать параметры метода в строку, переименовать already_existing_user в user

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -11,13 +11,10 @@ class AuthService:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def register_user(
-        self,
-        user_data: UserCreate,
-    ) -> User:
+    async def register_user(self, user_data: UserCreate) -> User:
         user_service = get_user_service(self.session)
-        already_existing_user = await user_service.get_user(username=user_data.username)
-        if already_existing_user:
+        user = await user_service.get_user(username=user_data.username)
+        if user:
             raise HTTPException(
                 status_code=status.HTTP_409_CONFLICT,
                 detail="The user with this username already exists",
