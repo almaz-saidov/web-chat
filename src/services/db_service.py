3235820b78657353996d99051378751fa5af922f
Almaz Saidov <almaz.saidov.03@bk.ru>
2026-01-31 13:28:29 +0300

    Добавить абстрактный сервис для работы с БД

---
+++ src/services/db_service.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from typing import Generic, TypeVar
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.repositories import Repository
+
+REPOSITORY_TYPE = TypeVar("REPOSITORY_TYPE", bound=Repository)
+
+
+class DbService(ABC, Generic[REPOSITORY_TYPE]):
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.repository: REPOSITORY_TYPE = self._create_repository()
+
+    @abstractmethod
+    def _create_repository(self) -> REPOSITORY_TYPE:
+        raise NotImplementedError("Необходимо определить self.repository")
