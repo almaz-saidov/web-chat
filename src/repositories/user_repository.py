ffc36c9f78dd63226dda30aa27a7e38ec54fb435
Almaz Saidov <almaz.saidov.03@bk.ru>
2026-01-31 13:27:41 +0300

    Сделать поиск по фильтрам

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -11,11 +11,8 @@ class UserRepository(
 ):
     cls_model = User
 
-    async def get_by_username(self, username: str) -> Optional[User]:
-        return await self.select_one(username=username)
-
-    async def get_by_id(self, user_id: uuid.UUID) -> Optional[User]:
-        return await self.select_one(id=user_id)
+    async def get_user(self, **filter_by) -> Optional[User]:
+        return await self.select_one(**filter_by)
 
     async def create_user(self, data: dict[str, str]) -> User:
         return await self.insert_one(data)
