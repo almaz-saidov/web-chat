135685a10d065205781d512b1430175ce0cc5aa1
Almaz <almaz.saidov.03@bk.ru>
2026-02-27 14:17:07 +0300

    Добавить тайп хинт

--- src/api/routes/websocket.py
+++ src/api/routes/websocket.py
@@ -12,5 +12,5 @@ async def websocket_endpoint(
     websocket: WebSocket,
     websocket_service: WebSocketService = Depends(get_websocket_service),
     user: UserSchema = Depends(get_current_user_from_ws),
-):
+) -> None:
     await websocket_service.handle_websocket(websocket=websocket, username=user.username)
