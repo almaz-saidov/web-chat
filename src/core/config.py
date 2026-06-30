cf93a267339f68d66f2dda9e32bc115e694c265d
Almaz <almaz.saidov.03@bk.ru>
2026-06-30 13:16:31 +0300

    Убрать Field у текстовых полей без ограничений

--- src/core/config.py
+++ src/core/config.py
@@ -11,18 +11,18 @@ MAX_PORT_NUMBER: Final = 65535
 
 
 class Settings(BaseSettings):
-    SERVER_HOST: str = Field(default="0.0.0.0")
+    SERVER_HOST: str = "0.0.0.0"
     SERVER_PORT: int = Field(default=8000, gt=0, le=MAX_PORT_NUMBER)
 
-    POSTGRES_HOST: str = Field(default="web-chat-db")
+    POSTGRES_HOST: str = "web-chat-db"
     POSTGRES_PORT: int = Field(default=5432, gt=0, le=MAX_PORT_NUMBER)
-    POSTGRES_USER: str = Field(default="db_user")
-    POSTGRES_PASSWORD: str = Field(default="db_pass")
-    POSTGRES_DB: str = Field(default="db_name")
+    POSTGRES_USER: str = "db_user"
+    POSTGRES_PASSWORD: str = "db_pass"
+    POSTGRES_DB: str = "db_name"
 
-    PRIVATE_KEY_PATH: Path = Field(default=Path(os.path.join(BASE_DIR, "core", "certs", "jwt-private.pem")))
-    PUBLIC_KEY_PATH: Path = Field(default=Path(os.path.join(BASE_DIR, "core", "certs", "jwt-public.pem")))
-    ALGORITHM: str = Field(default="RS256")
+    PRIVATE_KEY_PATH: Path = Path(os.path.join(BASE_DIR, "core", "certs", "jwt-private.pem"))
+    PUBLIC_KEY_PATH: Path = Path(os.path.join(BASE_DIR, "core", "certs", "jwt-public.pem"))
+    ALGORITHM: str = "RS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = Field(default=15, gt=0)
     REFRESH_TOKEN_EXPIRE_DAYS: int = Field(default=30, gt=0)
 
